a88b7ae0413fbdfd239afdb4cca2f7b7067650d2
moussacode <samouh269@gmail.com>
2026-09-05 15:43:10 +0000

    first commit

---
+++ src/main/java/sn/jappo/jappo_backend/JappoBackendApplication.java
@@ -0,0 +1,13 @@
+package sn.jappo.jappo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JappoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JappoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/jappo/jappo_backend/JappoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.jappo.jappo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JappoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
